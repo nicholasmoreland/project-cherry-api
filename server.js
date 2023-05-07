aeb0a0ccfa05d5a016f412376e1d78f9e45a1af5
nicholasmoreland <nmoreland18@outlook.com>
2023-05-07 15:33:35 -0500

    update: add cors to connect frontend to backend

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 
@@ -11,12 +12,15 @@ const db = mongoose.connection;
 db.on('error', (error) => console.log(error));
 db.once('open', () => console.log('Connected to database'));
 
+/* Enable CORS for all routes */
+app.use(cors());
+
 /* Parse incoming JSON data */
 app.use(express.json());
 
 /* User routes */
 const usersRouter = require('./routes/users');
-app.use('/users', usersRouter)
+app.use('/users', usersRouter);
 
 /* Starting up server */
 app.listen(port, () => {
